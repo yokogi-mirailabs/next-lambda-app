f070dd28336614c3579deb54670c5094fc74d44b
yokogi-mirailabs <yokogi@mirailabs.co.jp>
2026-01-14 05:55:54 +0000

    feat: Docker image deploy with Prisma Accelerate

--- backend/src/lib/prisma.ts
+++ backend/src/lib/prisma.ts
@@ -1,11 +1,4 @@
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
-declare global {
-  var prisma: PrismaClient | undefined
-}
-
-export const prisma = global.prisma || new PrismaClient()
-
-if (process.env.NODE_ENV !== "production") {
-  global.prisma = prisma
-}
+export const prisma = new PrismaClient().$extends(withAccelerate());
